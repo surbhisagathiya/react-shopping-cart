b5b954b8df130ca9ec752c4f59e404a6e92e2e7e
surbhi <surbhisaga337865@gmail.com>
2022-05-27 16:52:29 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+function App() {
+  return (
+    <div className="grid-container">
+      <div className="header">
+        <a href="/">Shopping Cart</a>
+      </div>
+      <div className="main">
+        product List
+      </div>
+      <div className="footer">
+        All right reservesd
+      </div>
+    </div>
+  );
+}
+
+export default App;
